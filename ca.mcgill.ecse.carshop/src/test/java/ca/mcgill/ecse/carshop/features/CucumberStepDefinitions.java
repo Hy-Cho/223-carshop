6406b4dc642f7fbb8e6a42236f98c097c21776a8
sami <77645479+samiad2788@users.noreply.github.com>
2021-03-12 18:34:24 -0400

    lala

--- ca.mcgill.ecse.carshop/src/test/java/ca/mcgill/ecse/carshop/features/CucumberStepDefinitions.java
+++ ca.mcgill.ecse.carshop/src/test/java/ca/mcgill/ecse/carshop/features/CucumberStepDefinitions.java
@@ -200,7 +200,7 @@ public class CucumberStepDefinitions {
 		}
 	}
 	@When("{string} initiates the update of the service {string} to name {string}, duration {string}, belonging to the garage of {string} technician")
-	public void updateService(String username, String oldName, String newName, String duration, String garageStr) throws Exception {
+	public void updateService(String username, String oldName, String newName, String duration, String garageStr) throws InvalidInputException {
 		try	{
 			Garage garage = getGarageOfTechnician(getTechnicianTypeFromString(garageStr));
 			this.oldServiceName = oldName;
