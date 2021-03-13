d8c54d61e234573b9f189db2debb4c25cabb57da
sami <77645479+samiad2788@users.noreply.github.com>
2021-03-12 23:52:38 -0400

    Update CucumberStepDefinitions.java

--- ca.mcgill.ecse.carshop/src/test/java/ca/mcgill/ecse/carshop/features/CucumberStepDefinitions.java
+++ ca.mcgill.ecse.carshop/src/test/java/ca/mcgill/ecse/carshop/features/CucumberStepDefinitions.java
@@ -134,7 +134,58 @@ public class CucumberStepDefinitions {
 
 	// end of sign up code
 	
-	/*
+	//Start of Update account code
+	
+	@Given("an owner account exists in the system with username {string} and password {string}")
+	public void an_owner_account_exists_in_the_system_with_username_and_password(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("the following customers exist in the system:")
+	public void the_following_customers_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+	    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+	    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+	    //
+	    // For other transformations you can register a DataTableType.
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("the following technicians exist in the system:")
+	public void the_following_technicians_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+	    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+	    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+	    //
+	    // For other transformations you can register a DataTableType.
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("each technician has their own garage")
+	public void each_technician_has_their_own_garage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the user tries to update account with a new username {string} and password {string}")
+	public void the_user_tries_to_update_account_with_a_new_username_and_password(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the account shall not be updated")
+	public void the_account_shall_not_be_updated() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	//End of update account code
+	
+	
 	@Given("an owner account exists in the system")
 	public void thereIsAnOwner()  {
 		Owner owner = new Owner("owner", "password", this.carshop);
@@ -296,7 +347,7 @@ public class CucumberStepDefinitions {
 		this.oldServiceName = null;
 	}
 	
-	*/
+	
 	
 	@After
 	public void tearDown() {
