43a5e9fc4e9a3b401df5cdc2311a61e2c93005f8
sami <77645479+samiad2788@users.noreply.github.com>
2021-04-11 15:16:22 -0300

    Create CarshopPage.java

---
+++ ca.mcgill.ecse.carshop/src/main/java/ca/mcgill/ecse/carshop/view/CarshopPage.java
@@ -0,0 +1,11 @@
+package ca.mcgill.ecse.carshop.view;
+
+import javax.swing.JFrame;
+
+public class CarshopPage extends JFrame {
+	private static final long serialVersionUID = -5633915762703837868L;
+	public CarshopPage() {
+		//initComponents();
+		//refreshData();
+	}
+}
